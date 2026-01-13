0e52eefb854b9f3ddb8c51b09c86482bd11dc595
Mysh! <me@mysh.dev>
2026-01-13 19:49:49 +0100

    fix(bitrate): check that we don't pass negative number into make()
    See details in the diff.

--- gumbleutil/bitrate.go
+++ gumbleutil/bitrate.go
@@ -13,6 +13,15 @@ var autoBitrate = &Listener{
 			interval := e.Client.Config.AudioInterval
 			dataBytes := (*e.MaximumBitrate / (8 * (int(time.Second/interval) + safety))) - 32 - 10
 
+			// If we use default config and specify maximum bitrate of <35000, then we get a negative number
+			// that is then passed into a make function in opus_nonshared that then panics.
+			//
+			// As a hack - let's set a minimum of 6 bytes for AudioDataBytes 
+			// (lowest value that worked in manual testing)
+			if dataBytes < 6 {
+				dataBytes = 6
+			}
+			
 			e.Client.Config.AudioDataBytes = dataBytes
 		}
 	},
